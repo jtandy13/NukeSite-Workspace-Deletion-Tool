d65ddcd63b6bf2e9a0a628308de31aa201b96af5
jtandy13 <james.tandy@gmail.com>
2016-12-17 23:03:07 +1100

    Project files

---
+++ NukeSite/NukeSpace/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IManage;
+using System.IO;
+
+namespace NukeSpace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NukeSetting myNuke = new NukeSetting();
+            this.DataContext = myNuke;
+        }
+
+        private void NukeButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Connect to the data context
+            NukeSetting dc = this.DataContext as NukeSetting;
+            
+            //Connect to the iManage database
+            IManDMS myDMS = new ManDMS();
+            IManSession mySess = myDMS.Sessions.Add(dc.serverName);
+           
+            
+            try
+            {//Should be removed in the final product
+                mySess.TrustedLogin();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            IManDatabase myDB = mySess.Databases.ItemByName(dc.DBname);
+
+            //Create a list to hold all the folder ids
+            List<int> folderList = new List<int>();
+
+            //Track status of csv file opening
+            Boolean fileStatus = false;
+
+            //Open the csv file and read the contents
+            try
+            {
+                var csvReader = new StreamReader(File.OpenRead(@dc.csvPath));
+                fileStatus = true;
+                //Populate folderList with data from the csv
+                while (!csvReader.EndOfStream)
+                {
+                    var line = csvReader.ReadLine();
+                    int id;
+                    Boolean success = Int32.TryParse(line, out id);
+                    folderList.Add(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //Only run the delete loop if the csv can be opened
+            if(fileStatus == true)
+            {
+                try
+                {
+                    foreach (int folder in folderList)
+                    {
+                        myDB.DeleteWorkspace(folder);
+                    }
+                    MessageBox.Show("All workspaces deleted successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ NukeSite/NukeSpace/NukeSetting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace NukeSpace
+{
+    class NukeSetting 
+    {
+        private string _DBname;
+        private string _serverName;
+        private string _csvPath;
+
+        public string DBname
+        {
+            get { return _DBname; }
+            set
+            {
+                if (_DBname != value)
+                {
+                    _DBname = value;
+                   
+                }
+            }
+        }
+
+        public string serverName
+        {
+            get { return _serverName; }
+            set
+            {
+                if (_serverName != value)
+                {
+                    _serverName = value;
+                    
+                }
+            }
+        }
+        public string csvPath
+        {
+            get { return _csvPath; }
+            set
+            {
+                if (_csvPath != value)
+                {
+                    _csvPath = value;
+                    
+                }
+            }
+        }
+
+
+        public NukeSetting() { }
+    }
+}
